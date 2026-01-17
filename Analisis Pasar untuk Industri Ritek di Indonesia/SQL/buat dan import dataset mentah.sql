170d39c7999732e55575d67f6ee604d612558b6b
Dhika Ramadhan Saputra <dhikaramadhansaputra@gmail.com>
2026-01-17 17:19:39 +0700

    abis nyicil preprocessing

--- Analisis Pasar untuk Industri Ritek di Indonesia/SQL/buat dan import dataset mentah.sql
+++ Analisis Pasar untuk Industri Ritek di Indonesia/SQL/buat dan import dataset mentah.sql
@@ -26,5 +26,15 @@ into table ritel_indonesia.raw_dataset
 fields terminated by ";"
 ignore 1 lines;
 
-select * from raw_dataset;
+-- DELETE ALL DATA FROM DATABASE (BCS THE DATE COLUMN HASNT BEEN FOMRATED)
+truncate table ritel_indonesia.raw_dataset;
+alter table ritel_indonesia.raw_dataset modify column Date varchar(10);
+SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'raw_dataset';
+update ritel_indonesia.raw_dataset set Date = str_to_date(Date, '%d/%m/%Y');
+alter table ritel_indonesia.raw_dataset modify column Date date;
+
+select * from raw_dataset where year(Date) = 2024 and month(Date) = 4;
+
+
+
 
